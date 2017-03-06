1c6f4665a98be7965c8fe147a482a4be9189c91d
thyda_eng <thyda@instedd.org>
2017-03-06 13:46:08 +0700

    fix deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,12 +27,8 @@ set :rbenv_roles, :all # default value
 
 set :application, 'khmer-writing'
 set :repo_url, 'https://github.com/ilabsea/khmer-writing'
-set :branch, :master
+set :branch, :develop
 
-# set :pty, true
-# set :passenger_restart_with_sudo, false
-# set :passenger_restart_command, "touch "
-# set :passenger_restart_options, -> { "#{release_path.join('tmp/restart.txt')}" }
 
 namespace :deploy do
   after :restart, :clear_cache do
@@ -47,81 +43,5 @@ end
 
 
 
-#for sidekiq upstart
-
-namespace :sidekiq do
-  task :quiet do
-    on roles(:app) do
-      # Horrible hack to get PID without having to use terrible PID files
-      puts "Killing sidekiq-upstart process "
-      puts capture("kill -USR1 $(sudo initctl status sidekiq-upstart-main | grep /running | awk '{print $NF}') || :")
-      puts capture("sudo initctl stop sidekiq-upstart-main")
-
-      puts capture("kill -USR1 $(sudo initctl status sidekiq-upstart-cron | grep /running | awk '{print $NF}') || :")
-      puts capture("sudo initctl stop sidekiq-upstart-cron")
-
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      # execute :sudo, :initctl, '--system' ,:restart, :'sidekiq-upstart'
-      puts 'Restarting sidekiq-upstart-cron project'
-      puts capture("sudo initctl start sidekiq-upstart-main")
-      puts capture("sudo initctl start sidekiq-upstart-cron")
-    end
-  end
-end
-
-# after 'deploy:starting', 'sidekiq:quiet'
-# after 'deploy:reverted', 'sidekiq:restart'
-# after 'deploy:published', 'sidekiq:restart'
-
-# namespace :monit do
-#   task :unmonitor do
-#     on roles(:app) do
-#       #execute :sudo, :monit, :unmonitor, :all
-#       puts capture("sudo monit unmonitor all")
-#     end
-#   end
-#   task :monitor do
-#     on roles(:app) do
-#       #execute :sudo, :monit, :monitor, :all
-#       puts capture("sudo monit monitor all")
-#     end
-#   end
-# end
-
-# before 'deploy:starting', 'monit:unmonitor'
-# after 'deploy:finished', 'monit:monitor'
-
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_files, %w{config/database.yml config/setting.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :linked_files, fetch(:linked_files, []).push('config/setting.yml', 'config/database.yml')
